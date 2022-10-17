647724ebacb13d4548c2cf0dc5e01d44ab879f51
Emily <em.smolka@gmail.com>
2022-10-17 13:19:16 -0400

    created thoughts model

---
+++ Models/thought.js
@@ -0,0 +1,24 @@
+//create model
+const { Schema, model } = require("mongoose");
+
+//create schema
+const ThoughtSchema = new Schema({
+  username: {
+    type: String,
+  },
+  thoughtText: {
+    type: String,
+    min: 1,
+    max: 280,
+  },
+  createdAt{
+    type: Number,
+    default: (new Date()).getTime(),
+  },
+  reactions{
+    type: Schema.Types.ObjectId,
+      ref: "Reactions",
+  }
+});
+const Thought = model("Thought", ThoughtSchema);
+module.exports = Thought;
